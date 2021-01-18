da453eea5740320438af662ba17b49e1a5770868
jhonatantiradoc <jhonatantirado4@gmail.com>
2021-01-18 18:08:36 -0500

    Primer proyecto - Calculadora básica

---
+++ PrimerosPasosCsharp/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace PrimerosPasosCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtNum1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtNum2 = new System.Windows.Forms.TextBox();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSuma = new System.Windows.Forms.TextBox();
+            this.txtResta = new System.Windows.Forms.TextBox();
+            this.txtMulti = new System.Windows.Forms.TextBox();
+            this.txtDivision = new System.Windows.Forms.TextBox();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1:";
+            // 
+            // TxtNum1
+            // 
+            this.TxtNum1.Location = new System.Drawing.Point(157, 28);
+            this.TxtNum1.Name = "TxtNum1";
+            this.TxtNum1.Size = new System.Drawing.Size(100, 20);
+            this.TxtNum1.TabIndex = 1;
+            this.TxtNum1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNum1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Número 2:";
+            // 
+            // TxtNum2
+            // 
+            this.TxtNum2.Location = new System.Drawing.Point(157, 96);
+            this.TxtNum2.Name = "TxtNum2";
+            this.TxtNum2.Size = new System.Drawing.Size(100, 20);
+            this.TxtNum2.TabIndex = 3;
+            this.TxtNum2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNum2_KeyPress);
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BtnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCalcular.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCalcular.Location = new System.Drawing.Point(52, 158);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(88, 36);
+            this.BtnCalcular.TabIndex = 4;
+            this.BtnCalcular.Text = "CALCULAR";
+            this.BtnCalcular.UseVisualStyleBackColor = false;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Suma:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 291);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Resta:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 336);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Multiplicación:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 379);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "División:";
+            // 
+            // txtSuma
+            // 
+            this.txtSuma.Enabled = false;
+            this.txtSuma.Location = new System.Drawing.Point(157, 230);
+            this.txtSuma.Name = "txtSuma";
+            this.txtSuma.Size = new System.Drawing.Size(100, 20);
+            this.txtSuma.TabIndex = 9;
+            // 
+            // txtResta
+            // 
+            this.txtResta.Enabled = false;
+            this.txtResta.Location = new System.Drawing.Point(157, 284);
+            this.txtResta.Name = "txtResta";
+            this.txtResta.Size = new System.Drawing.Size(100, 20);
+            this.txtResta.TabIndex = 10;
+            // 
+            // txtMulti
+            // 
+            this.txtMulti.Enabled = false;
+            this.txtMulti.Location = new System.Drawing.Point(157, 329);
+            this.txtMulti.Name = "txtMulti";
+            this.txtMulti.Size = new System.Drawing.Size(100, 20);
+            this.txtMulti.TabIndex = 11;
+            // 
+            // txtDivision
+            // 
+            this.txtDivision.Enabled = false;
+            this.txtDivision.Location = new System.Drawing.Point(157, 372);
+            this.txtDivision.Name = "txtDivision";
+            this.txtDivision.Size = new System.Drawing.Size(100, 20);
+            this.txtDivision.TabIndex = 12;
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.BackColor = System.Drawing.Color.Aqua;
+            this.BtnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLimpiar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpiar.Location = new System.Drawing.Point(167, 158);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(90, 36);
+            this.BtnLimpiar.TabIndex = 13;
+            this.BtnLimpiar.Text = "LIMPIAR";
+            this.BtnLimpiar.UseVisualStyleBackColor = false;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(326, 448);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.txtDivision);
+            this.Controls.Add(this.txtMulti);
+            this.Controls.Add(this.txtResta);
+            this.Controls.Add(this.txtSuma);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.TxtNum2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtNum1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora Básica";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtNum1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtNum2;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSuma;
+        private System.Windows.Forms.TextBox txtResta;
+        private System.Windows.Forms.TextBox txtMulti;
+        private System.Windows.Forms.TextBox txtDivision;
+        private System.Windows.Forms.Button BtnLimpiar;
+    }
+}
+
---
+++ PrimerosPasosCsharp/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimerosPasosCsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            if (TxtNum1.Text != "" && TxtNum2.Text != "")
+            {
+                double num1, num2, suma, resta, multiplicacion, division;
+                num1 = Convert.ToDouble(TxtNum1.Text);
+                num2 = Convert.ToDouble(TxtNum2.Text);
+                suma = num1 + num2;
+                resta = num1 - num2;
+                multiplicacion = num1 * num2;
+                division = num1 / num2;
+                txtSuma.Text = suma.ToString();
+                txtResta.Text = "" + resta;
+                txtMulti.Text = Convert.ToString(multiplicacion);
+                txtDivision.Text = division.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese los valores", "Aviso del Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                TxtNum1.Focus();
+            }
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            TxtNum1.Clear();
+            TxtNum2.Text = "";
+            txtSuma.Text = "";
+            txtResta.Text = "";
+            txtMulti.Text = "";
+            txtDivision.Text = "";
+            TxtNum1.Focus();
+        }
+
+        private void TxtNum1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TxtNum2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
